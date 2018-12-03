4153eea81fc5b9d34a361d5641a401ef6a006362
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-12-03 20:45:16 +0800

    ok

--- LearnPCL/main.cpp
+++ LearnPCL/main.cpp
@@ -1,43 +1,33 @@
 ﻿#include <iostream>
+#include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
-#include <pcl/filters/passthrough.h>
+#include <pcl/filters/voxel_grid.h>
 
 int main (int argc, char** argv)
 {
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PCLPointCloud2::Ptr cloud (new pcl::PCLPointCloud2 ());
+    pcl::PCLPointCloud2::Ptr cloud_filtered (new pcl::PCLPointCloud2 ());
 
     // Fill in the cloud data
-    cloud->width  = 5;
-    cloud->height = 1;
-    cloud->points.resize (cloud->width * cloud->height);
+    pcl::PCDReader reader;
+    // Replace the path below with the path where you saved your file
+    reader.read ("../data/table_scene_lms400.pcd", *cloud); // Remember to download the file first!
 
-    for (size_t i = 0; i < cloud->points.size (); ++i)
-    {
-        cloud->points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
-        cloud->points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
-        cloud->points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
-    }
-
-    std::cout << "Cloud before filtering: " << std::endl;
-    for (size_t i = 0; i < cloud->points.size (); ++i)
-        std::cout << "    " << cloud->points[i].x << " "
-                        << cloud->points[i].y << " "
-                        << cloud->points[i].z << std::endl;
+    std::cerr << "PointCloud before filtering: " << cloud->width * cloud->height
+       << " data points (" << pcl::getFieldsList (*cloud) << ").";
 
     // Create the filtering object
-    pcl::PassThrough<pcl::PointXYZ> pass;
-    pass.setInputCloud (cloud);
-    pass.setFilterFieldName ("z");
-    pass.setFilterLimits (0.0, 800.0);
-//    pass.setFilterLimitsNegative (true);
-    pass.filter (*cloud_filtered);
+    pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+    sor.setInputCloud (cloud);
+    sor.setLeafSize (0.01f, 0.01f, 0.01f);
+    sor.filter (*cloud_filtered);
 
-    std::cout << "Cloud after filtering: " << std::endl;
-    for (size_t i = 0; i < cloud_filtered->points.size (); ++i)
-    std::cout << "    " << cloud_filtered->points[i].x << " "
-                        << cloud_filtered->points[i].y << " "
-                        << cloud_filtered->points[i].z << std::endl;
+    std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height
+       << " data points (" << pcl::getFieldsList (*cloud_filtered) << ").";
+
+    pcl::PCDWriter writer;
+    writer.write ("../data/table_scene_lms400_downsampled.pcd", *cloud_filtered,
+         Eigen::Vector4f::Zero (), Eigen::Quaternionf::Identity (), false);
 
     return 0;
 }
