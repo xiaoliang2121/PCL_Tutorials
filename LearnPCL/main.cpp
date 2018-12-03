cf61dea0efa8c30d403032cd274a8cf8b895df86
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-12-03 22:16:30 +0800

    ok

--- LearnPCL/main.cpp
+++ LearnPCL/main.cpp
@@ -2,51 +2,92 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/ModelCoefficients.h>
-#include <pcl/filters/project_inliers.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/extract_indices.h>
 
 int main (int argc, char** argv)
 {
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(
-                new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_projected(
-                new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PCLPointCloud2::Ptr
+            cloud_blob(new pcl::PCLPointCloud2),
+            cloud_filtered_blob(new pcl::PCLPointCloud2);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr
+            cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>),
+            cloud_p(new pcl::PointCloud<pcl::PointXYZ>),
+            cloud_f(new pcl::PointCloud<pcl::PointXYZ>);
 
-    cloud->width = 5;
-    cloud->height = 1;
-    cloud->points.resize(cloud->width*cloud->height);
-    for (size_t i = 0; i < cloud->points.size (); ++i)
-    {
-        cloud->points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
-        cloud->points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
-        cloud->points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
-    }
+    // Fill in the cloud data
+    pcl::PCDReader reader;
+    reader.read("../data/table_scene_lms400.pcd",*cloud_blob);
 
-    std::cerr << "Cloud before projection: " << std::endl;
-    for (size_t i = 0; i < cloud->points.size (); ++i)
-        std::cerr << "    " << cloud->points[i].x << " "
-                        << cloud->points[i].y << " "
-                        << cloud->points[i].z << std::endl;
+    std::cerr << "PointCloud before filtering: " << cloud_blob->width * cloud_blob->height
+              << " data points." << std::endl;
 
-    // Create a set of planar coefficients with X=Y=0,Z=1
-    pcl::ModelCoefficients::Ptr coefficients(
-                new pcl::ModelCoefficients());
-    coefficients->values.resize(4);
-    coefficients->values[0] = coefficients->values[1] = 0;
-    coefficients->values[2] = 1.0;
-    coefficients->values[3] = -1.0;
+    // Create the filtering object: downsample the dataset using a leaf size of 1cm
+    pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+    sor.setInputCloud(cloud_blob);
+    sor.setLeafSize(0.01f,0.01f,0.01f);
+    sor.filter(*cloud_filtered_blob);
+
+    // Convert to the templated PointCloud
+    pcl::fromPCLPointCloud2(*cloud_filtered_blob,*cloud_filtered);
+    std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height
+              << " data points." << std::endl;
+
+    // Write the downsampled version to disk
+    pcl::PCDWriter writer;
+    writer.write<pcl::PointXYZ> ("table_scene_lms400_downsampled.pcd",
+                               *cloud_filtered, false);
+
+    pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients());
+    pcl::PointIndices::Ptr inliers(new pcl::PointIndices());
+
+    // Create the segmentation object
+    pcl::SACSegmentation<pcl::PointXYZ> seg;
+    seg.setOptimizeCoefficients(true);
+    seg.setModelType(pcl::SACMODEL_PLANE);
+    seg.setMethodType(pcl::SAC_RANSAC);
+    seg.setMaxIterations(1000);
+    seg.setDistanceThreshold(0.01);
 
     // Create the filtering object
-    pcl::ProjectInliers<pcl::PointXYZ> proj;
-    proj.setModelType(pcl::SACMODEL_PLANE);
-    proj.setInputCloud(cloud);
-    proj.setModelCoefficients(coefficients);
-    proj.filter(*cloud_projected);
+    pcl::ExtractIndices<pcl::PointXYZ> extract;
+    int i = 0, nr_points = (int)cloud_filtered->points.size();
+    // While 30% of the original cloud is still there
+    while(cloud_filtered->points.size() > 0.3*nr_points)
+    {
+        // Segment the largest planar component from the remaining cloud
+        seg.setInputCloud(cloud_filtered);
+        seg.segment(*inliers, *coefficients);
+        if(inliers->indices.size() == 0)
+        {
+            std::cerr << "Could not estimate a planar model for the given dataset." << std::endl;
+            break;
+        }
 
-    std::cerr << "Cloud after projection: " << std::endl;
-    for (size_t i = 0; i < cloud_projected->points.size (); ++i)
-        std::cerr << "    " << cloud_projected->points[i].x << " "
-                        << cloud_projected->points[i].y << " "
-                        << cloud_projected->points[i].z << std::endl;
+        // Extract the inliers
+        extract.setInputCloud(cloud_filtered);
+        extract.setIndices(inliers);
+        extract.setNegative(false);
+        extract.filter(*cloud_p);
+        std::cerr << "PointCloud representing the planar component: "
+                  << cloud_p->width * cloud_p->height << " data points."
+                  << std::endl;
+
+        std::stringstream ss;
+        ss<<"table_scene_lms400_plane_"<<i<<".pcd";
+        writer.write<pcl::PointXYZ>(ss.str(),*cloud_p,false);
+
+        // Create the filtering object
+        extract.setNegative(true);
+        extract.filter(*cloud_f);
+        cloud_filtered.swap(cloud_f);
+
+        i++;
+    }
+
 
     return 0;
 }
