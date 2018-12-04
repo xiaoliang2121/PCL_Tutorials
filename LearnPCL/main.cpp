9d9e9d149e3566b29a6b31e9a6eb327cc6483421
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-12-04 20:05:30 +0800

    ok

--- LearnPCL/main.cpp
+++ LearnPCL/main.cpp
@@ -1,43 +1,97 @@
 ﻿#include <iostream>
 #include <pcl/point_types.h>
-#include <pcl/filters/passthrough.h>
+#include <pcl/io/pcd_io.h>
 
 int main (int argc, char** argv)
 {
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
-
-    // Fill in the cloud data
-    cloud->width  = 5;
-    cloud->height = 1;
-    cloud->points.resize (cloud->width * cloud->height);
-
-    for (size_t i = 0; i < cloud->points.size (); ++i)
+    if(argc != 2)
     {
-        cloud->points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
-        cloud->points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
-        cloud->points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
+        std::cerr<<"please specify command line arg '-f' or '-p'"<<std::endl;
+        exit(0);
     }
 
-    std::cout << "Cloud before filtering: " << std::endl;
-    for (size_t i = 0; i < cloud->points.size (); ++i)
-        std::cout << "    " << cloud->points[i].x << " "
-                        << cloud->points[i].y << " "
-                        << cloud->points[i].z << std::endl;
+    pcl::PointCloud<pcl::PointXYZ> cloud_a,cloud_b,cloud_c;
+    pcl::PointCloud<pcl::Normal> n_cloud_b;
+    pcl::PointCloud<pcl::PointNormal> p_n_cloud_c;
 
-    // Create the filtering object
-    pcl::PassThrough<pcl::PointXYZ> pass;
-    pass.setInputCloud (cloud);
-    pass.setFilterFieldName ("z");
-    pass.setFilterLimits (0.0, 800.0);
-//    pass.setFilterLimitsNegative (true);
-    pass.filter (*cloud_filtered);
+    // 创建点云
+    cloud_a.width=5;
+    cloud_a.height=cloud_b.height=n_cloud_b.height=1;
+    cloud_a.points.resize(cloud_a.width*cloud_a.height);
 
-    std::cout << "Cloud after filtering: " << std::endl;
-    for (size_t i = 0; i < cloud_filtered->points.size (); ++i)
-    std::cout << "    " << cloud_filtered->points[i].x << " "
-                        << cloud_filtered->points[i].y << " "
-                        << cloud_filtered->points[i].z << std::endl;
+    if(strcmp(argv[1],"-p")==0)
+    {
+        cloud_b.width=3;
+        cloud_b.points.resize(cloud_b.width*cloud_b.height);
+    }
+    else{
+        n_cloud_b.width=5;
+        n_cloud_b.points.resize(n_cloud_b.width*n_cloud_b.height);
+    }
 
-    return 0;
+    for(size_t i=0;i<cloud_a.points.size();++i)
+    {
+        cloud_a.points[i].x=1024*rand()/(RAND_MAX+1.0f);
+        cloud_a.points[i].y=1024*rand()/(RAND_MAX+1.0f);
+        cloud_a.points[i].z=1024*rand()/(RAND_MAX+1.0f);
+    }
+
+    if(strcmp(argv[1],"-p")==0)
+        for(size_t i=0;i<cloud_b.points.size();++i)
+        {
+            cloud_b.points[i].x=1024*rand()/(RAND_MAX+1.0f);
+            cloud_b.points[i].y=1024*rand()/(RAND_MAX+1.0f);
+            cloud_b.points[i].z=1024*rand()/(RAND_MAX+1.0f);
+        }
+    else
+        for(size_t i=0;i<n_cloud_b.points.size();++i)
+        {
+            n_cloud_b.points[i].normal[0]=1024*rand()/(RAND_MAX+1.0f);
+            n_cloud_b.points[i].normal[1]=1024*rand()/(RAND_MAX+1.0f);
+            n_cloud_b.points[i].normal[2]=1024*rand()/(RAND_MAX+1.0f);
+        }
+
+    std::cerr<<"Cloud A: "<<std::endl;
+    for(size_t i=0;i<cloud_a.points.size();++i)
+        std::cerr<<"    "<<cloud_a.points[i].x
+                <<" "<<cloud_a.points[i].y
+               <<" "<<cloud_a.points[i].z<<std::endl;
+
+    std::cerr<<"Cloud B: "<<std::endl;
+    if(strcmp(argv[1],"-p")==0)
+        for(size_t i=0;i<cloud_b.points.size();++i)
+            std::cerr<<"    "<<cloud_b.points[i].x
+                    <<" "<<cloud_b.points[i].y<<" "
+                   <<cloud_b.points[i].z<<std::endl;
+    else
+        for(size_t i=0;i<n_cloud_b.points.size();++i)
+            std::cerr<<"    "<<n_cloud_b.points[i].normal[0]
+                    <<" "<<n_cloud_b.points[i].normal[1]
+                   <<" "<<n_cloud_b.points[i].normal[2]<<std::endl;
+
+    //拷贝点云数据
+    if(strcmp(argv[1],"-p")==0)
+    {
+        cloud_c = cloud_a;
+        cloud_c += cloud_b;
+        std::cerr<<"Cloud C: "<<std::endl;
+        for(size_t i=0;i<cloud_c.points.size();++i)
+            std::cerr<<"    "<<cloud_c.points[i].x
+                    <<" "<<cloud_c.points[i].y
+                   <<" "<<cloud_c.points[i].z<<" "<<std::endl;
+    }
+    else
+    {
+        pcl::concatenateFields(cloud_a,n_cloud_b,p_n_cloud_c);
+        std::cerr<<"Cloud C: "<<std::endl;
+        for(size_t i=0;i<p_n_cloud_c.points.size();++i)
+            std::cerr<<"    "<<p_n_cloud_c.points[i].x
+                    <<" "<<p_n_cloud_c.points[i].y
+                   <<" "<<p_n_cloud_c.points[i].z
+                  <<" "<<p_n_cloud_c.points[i].normal[0]
+                 <<" "<<p_n_cloud_c.points[i].normal[1]
+                <<" "<<p_n_cloud_c.points[i].normal[2]<<std::endl;
+    }
+
+    return (0);
 }
