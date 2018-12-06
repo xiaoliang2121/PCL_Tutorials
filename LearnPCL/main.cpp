320dc9f0a48a3b136b011ed271c2b8acb2cefb42
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-12-06 15:37:55 +0800

    pcl plotter

--- LearnPCL/main.cpp
+++ LearnPCL/main.cpp
@@ -1,43 +1,118 @@
 ﻿#include <iostream>
-#include <pcl/point_types.h>
-#include <pcl/filters/passthrough.h>
+#include <vector>
+#include <utility>
+#include <cmath>
+#include <pcl/visualization/pcl_plotter.h>
+
+using namespace std;
+using namespace pcl::visualization;
+
+void
+generateData (double *ax, double *acos, double *asin, int numPoints)
+{
+  double inc = 7.5 / (numPoints - 1);
+  for (int i = 0; i < numPoints; ++i)
+  {
+    ax[i] = i*inc;
+    acos[i] = cos (i * inc);
+    asin[i] = sin (i * inc);
+  }
+}
+
+//.....................callback functions defining Y= f(X)....................
+double
+step (double val)
+{
+  if (val > 0)
+    return (double) (int) val;
+  else
+    return (double) ((int) val - 1);
+}
+
+double
+identit (double val)
+{
+  return val;
+}
+//............................................................................
 
 int main (int argc, char** argv)
 {
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+//    pcl::visualization::PCLPlotter *plotter =
+//            new pcl::visualization::PCLPlotter();
 
-    // Fill in the cloud data
-    cloud->width  = 5;
-    cloud->height = 1;
-    cloud->points.resize (cloud->width * cloud->height);
+//    std::vector<double> func1(3,0);
+//    func1[2] = 1;
 
-    for (size_t i = 0; i < cloud->points.size (); ++i)
-    {
-        cloud->points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
-        cloud->points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
-        cloud->points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
-    }
+//    plotter->addPlotData(func1,-10,10,"y=x^2");
 
-    std::cout << "Cloud before filtering: " << std::endl;
-    for (size_t i = 0; i < cloud->points.size (); ++i)
-        std::cout << "    " << cloud->points[i].x << " "
-                        << cloud->points[i].y << " "
-                        << cloud->points[i].z << std::endl;
+//    plotter->setWindowSize(800,600);
+//    plotter->setWindowName("PCL Plotter");
+//    plotter->plot();
 
-    // Create the filtering object
-    pcl::PassThrough<pcl::PointXYZ> pass;
-    pass.setInputCloud (cloud);
-    pass.setFilterFieldName ("z");
-    pass.setFilterLimits (0.0, 800.0);
-//    pass.setFilterLimitsNegative (true);
-    pass.filter (*cloud_filtered);
+    //defining a plotter
+      PCLPlotter *plotter = new PCLPlotter ("My Plotter");
 
-    std::cout << "Cloud after filtering: " << std::endl;
-    for (size_t i = 0; i < cloud_filtered->points.size (); ++i)
-    std::cout << "    " << cloud_filtered->points[i].x << " "
-                        << cloud_filtered->points[i].y << " "
-                        << cloud_filtered->points[i].z << std::endl;
+      //setting some properties
+      plotter->setShowLegend (true);
+
+      //generating point correspondances
+      int numPoints = 69;
+      double ax[100], acos[100], asin[100];
+      generateData (ax, acos, asin, numPoints);
+
+      //adding plot data
+      plotter->addPlotData (ax, acos, numPoints, "cos");
+      plotter->addPlotData (ax, asin, numPoints, "sin");
+
+      //display for 2 seconds
+      plotter->spinOnce (3000);
+      plotter->clearPlots ();
+
+
+      //...................plotting implicit functions and custom callbacks....................
+
+      //make a fixed axis
+      plotter->setYRange (-10, 10);
+
+      //defining polynomials
+      vector<double> func1 (1, 0);
+      func1[0] = 1; //y = 1
+      vector<double> func2 (3, 0);
+      func2[2] = 1; //y = x^2
+
+      plotter->addPlotData (std::make_pair (func1, func2), -10, 10, "y = 1/x^2", 100, vtkChart::POINTS);
+      plotter->spinOnce (2000);
+
+      plotter->addPlotData (func2, -10, 10, "y = x^2");
+      plotter->spinOnce (2000);
+
+      //callbacks
+      plotter->addPlotData(identit, -10, 10, "identity");
+      plotter->spinOnce (2000);
+
+      plotter->addPlotData (abs, -10, 10, "abs");
+      plotter->spinOnce (2000);
+
+      plotter->addPlotData (step, -10, 10, "step", 100, vtkChart::POINTS);
+      plotter->spinOnce (2000);
+
+      plotter->clearPlots ();
+
+      //........................A simple animation..............................
+      vector<double> fsq (3, 0);
+      fsq[2] = -100; //y = x^2
+      while (!plotter->wasStopped ())
+      {
+        if (fsq[2] == 100) fsq[2] = -100;
+        fsq[2]++;
+        char str[50];
+        sprintf (str, "y = %dx^2", (int) fsq[2]);
+        plotter->addPlotData (fsq, -10, 10, str);
+
+        plotter->spinOnce (100);
+        plotter->clearPlots ();
+      }
 
     return 0;
 }
