2b114ea7ac260ed1528a91fc3d3eebf793402e38
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-12-07 21:37:46 +0800

    ok

--- LearnPCL/main.cpp
+++ LearnPCL/main.cpp
@@ -1,35 +1,51 @@
 ﻿#include <iostream>
 #include <pcl/point_types.h>
-//#include <pcl/features/normal_3d_omp.h>
+#include <pcl/features/fpfh.h>
 #include <pcl/features/integral_image_normal.h>
 #include <pcl/io/pcd_io.h>
-#include <pcl/visualization/pcl_visualizer.h>
 
 int main (int argc, char** argv)
 {
-    pcl::PointCloud<pcl::PointXYZ>::Ptr
-            cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>());
 
-    pcl::io::loadPCDFile("../data/table_scene_mug_stereo_textured.pcd",*cloud);
+    pcl::io::loadPCDFile<pcl::PointXYZ>("../data/table_scene_mug_stereo_textured.pcd",*cloud);
+
+    std::cout<<"Cloud Point size: "<<cloud->points.size()<<std::endl;
 
     pcl::IntegralImageNormalEstimation<pcl::PointXYZ,pcl::Normal> ne;
-    ne.setNormalEstimationMethod(ne.AVERAGE_3D_GRADIENT);
+    ne.setNormalEstimationMethod (ne.AVERAGE_3D_GRADIENT);
     ne.setMaxDepthChangeFactor(0.02f);
     ne.setNormalSmoothingSize(10.0f);
     ne.setInputCloud(cloud);
+    ne.compute(*normals);
 
-    pcl::PointCloud<pcl::Normal>::Ptr
-            cloud_normals(new pcl::PointCloud<pcl::Normal>);
-    ne.compute(*cloud_normals);
+    // Create the FPFH estimation class, and pass the input dataset+normals to it
+    pcl::FPFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::FPFHSignature33> fpfh;
+    fpfh.setInputCloud (cloud);
+    fpfh.setInputNormals (normals);
+    // alternatively, if cloud is of tpe PointNormal, do fpfh.setInputNormals (cloud);
 
-    pcl::visualization::PCLVisualizer viewer("PCL Viewer");
-    viewer.setBackgroundColor(0.0,0.0,0.5);
-    viewer.addPointCloudNormals<pcl::PointXYZ,pcl::Normal>(cloud,cloud_normals);
+    // Create an empty kdtree representation, and pass it to the FPFH estimation object.
+    // Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr
+            tree(new pcl::search::KdTree<pcl::PointXYZ>);
 
-    while(!viewer.wasStopped())
-    {
-        viewer.spinOnce();
-    }
+    fpfh.setSearchMethod (tree);
+
+    // Output datasets
+    pcl::PointCloud<pcl::FPFHSignature33>::Ptr
+            fpfhs(new pcl::PointCloud<pcl::FPFHSignature33>());
+
+    // Use all neighbors in a sphere of radius 5cm
+    // IMPORTANT: the radius used here has to be larger than the radius used to estimate the surface normals!!!
+    fpfh.setRadiusSearch (0.05);
+
+    // Compute the features
+    fpfh.compute (*fpfhs);
+
+    // fpfhs->points.size () should have the same size as the input cloud->points.size ()*
+    std::cout<<"fpfhs Point size: "<<fpfhs->points.size()<<std::endl;
 
     return 0;
 }
