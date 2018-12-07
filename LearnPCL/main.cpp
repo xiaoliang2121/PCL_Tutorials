976e5d8298ee8761f5f1ced8b2fd0ffe4e5437e4
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-12-07 20:51:58 +0800

    ok

--- LearnPCL/main.cpp
+++ LearnPCL/main.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 #include <pcl/point_types.h>
-#include <pcl/features/normal_3d_omp.h>
+//#include <pcl/features/normal_3d_omp.h>
 #include <pcl/features/integral_image_normal.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/visualization/pcl_visualizer.h>
@@ -10,22 +10,20 @@ int main (int argc, char** argv)
     pcl::PointCloud<pcl::PointXYZ>::Ptr
             cloud (new pcl::PointCloud<pcl::PointXYZ>);
 
-    pcl::io::loadPCDFile("../data/table_scene_lms400.pcd",*cloud);
+    pcl::io::loadPCDFile("../data/table_scene_mug_stereo_textured.pcd",*cloud);
 
-    pcl::NormalEstimationOMP<pcl::PointXYZ,pcl::Normal> ne;
+    pcl::IntegralImageNormalEstimation<pcl::PointXYZ,pcl::Normal> ne;
+    ne.setNormalEstimationMethod(ne.AVERAGE_3D_GRADIENT);
+    ne.setMaxDepthChangeFactor(0.02f);
+    ne.setNormalSmoothingSize(10.0f);
     ne.setInputCloud(cloud);
-
-    pcl::search::KdTree<pcl::PointXYZ>::Ptr
-            tree(new pcl::search::KdTree<pcl::PointXYZ>());
-    ne.setSearchMethod(tree);
 
     pcl::PointCloud<pcl::Normal>::Ptr
             cloud_normals(new pcl::PointCloud<pcl::Normal>);
-    ne.setRadiusSearch(0.03);
     ne.compute(*cloud_normals);
 
     pcl::visualization::PCLVisualizer viewer("PCL Viewer");
-    viewer.setBackgroundColor(0.0,0.0,0.0);
+    viewer.setBackgroundColor(0.0,0.0,0.5);
     viewer.addPointCloudNormals<pcl::PointXYZ,pcl::Normal>(cloud,cloud_normals);
 
     while(!viewer.wasStopped())
